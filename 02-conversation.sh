7413f412a36374071c270b458a7abf3a477bed05
udaykiran <samanthulaudaykiran@gmail.com>
2025-10-09 22:19:23 +0530

    code pushing

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rmesh:: Hi suresh, how are you?"
+echo "suresh:: hello ramesh, iam fine ,how are you?"
+echo "ramesh :: what are you learning recently?"
+echo "suresh:: iam learning shell scripting ramesh"
