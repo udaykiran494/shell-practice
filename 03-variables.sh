70ecd5ed76f8f269f4f09db4e82cf2bc0134209b
udaykiran <samanthulaudaykiran@gmail.com>
2025-10-11 10:49:10 +0530

    code pushing

---
+++ 03-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSON1=UDAY
+PERSON2=KIRAN
+
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: hello $PERSON1, iam fine ,how are you?"
+echo "$PERSON1 :: what are you learning recently?"
+echo "$PERSON2:: iam learning shell scripting $PERSON1"
+
+
+#variable refering
+#var_name=value
+#$var_name=
+#${var_name}
