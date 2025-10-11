d735d50d378e63efb3e086aee75d04b3117ffabc
udaykiran <samanthulaudaykiran@gmail.com>
2025-10-11 11:19:40 +0530

    code pushing

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#arguments
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: welcome to india"
+echo "$PERSON2:: thank you"
+echo "$PERSON1::  so, we have a deal"
+echo "$PERSON2:: yes,if it is ok"
+
---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ echo "please enyour PIN number"
+ read PIN_NUMBER # HERE PIN_NUMBER IS VARIABLE"
+
+
+ echo   "pin enter is : $PIN_NUMBER"
+ 
