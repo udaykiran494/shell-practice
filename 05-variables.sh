2bc33d3d2f67aa9f230652dda623098a6afc5d4c
udaykiran <samanthulaudaykiran@gmail.com>
2025-10-11 11:42:46 +0530

    code pushing

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "please enter your PIN number"
-read  PIN_NUMBER # HERE PIN_NUMBER IS VARIABLE
+read -s PIN_NUMBER # HERE PIN_NUMBER IS VARIABLE
 
 
 echo "pin enter is: $PIN_NUMBER"
