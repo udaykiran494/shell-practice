983bd1e2df09582e2f7c54c7600860ca577500e4
udaykiran <samanthulaudaykiran@gmail.com>
2025-10-12 12:33:26 +0530

    code pushing

--- 08-variables.sh
+++ 08-variables.sh
@@ -3,3 +3,6 @@
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo " script executed name: $0"
+echo "current directory: $PWD"
+echo "who is running this : $USER"
+echo "Home directory of user: $HOME"
